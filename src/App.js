c0aeb9cdba6ba7803966aa79ccf7e03a8027287e
yuvalkansal <yuvalkansal@gmail.com>
2019-11-10 00:08:29 +0800

    Changed the hyperlinks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { whileStatement } from '@babel/types';
 
 class App extends React.Component {
   constructor(props){
     super(props)
+    this.handleInput = this.handleInput.bind(this);
     this.state = {value: ''}
   }
-  display(){
-    return alert("The submit button has been pressed")
+  display(x){
+    return alert('The',x.value,'button has been pressed')
+  }
+  handleInput(e){
+    this.setState({value: e.target.value})
   }
   render()
   {
@@ -17,14 +22,15 @@ class App extends React.Component {
       <div className = "test">
       <a 
       href = "https://gmail.com"
-      
+      style = {{paddingLeft: 13,textDecoration: 'none'}}
       >
-        Gmail&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Gmail
       </a>
       <a
       href = "https://www.google.com/imghp?hl=en"
+      style = {{paddingLeft: 13,textDecoration: 'none'}}
       >
-        Images&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Images
       </a>
       
       </div>
@@ -33,12 +39,12 @@ class App extends React.Component {
         <img src={"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"} className="App-logo" alt="logo" />
         <form>
           <label>
-            <input type = 'text' />
- 
-            <button onClick = {this.display}>
+            <input type = 'number' onChange = {this.handleInput} />
+            </label>
+            <button onClick = {this.display = this.state}>
               Submit
-              </button>
-          </label>
+            </button>
+          
         </form>
         <br />
         <select>
